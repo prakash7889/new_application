bac622139a3008d4719bfe860407989bd3514fff
prakash7889 <bprakashjk2000@gmail.com>
2024-12-27 16:04:26 +0530

    first push

---
+++ lib/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("My New App" , style: TextStyle(color: Colors.white),),
+        leading: Icon(Icons.home , color: Colors.white, size: 40,),),
+
+    );
+  }
+}
